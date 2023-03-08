e7bb46e73246e28400c2fa5bb5b1dee78b10131d
Veena Krishna Sandana <43022790+Veenakrishna2313@users.noreply.github.com>
2023-03-07 23:30:40 -0800

    Dice Held and value check done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { nanoid } from 'nanoid'
 import './App.css'
 import Dice from './Components/dice'
@@ -6,41 +6,61 @@ import Dice from './Components/dice'
 
 
 function App() {
+
+  const generateNewDice=()=>{
+ return {value:Math.floor(Math.random()*(7-1)+1), isHeld:false, id:nanoid()}
+}
    const allNewDice=()=>{
      const numArray=[];
 
         for(let i=0;i<10;i++){
-        let random= Math.floor(Math.random()*(7-1)+1)
-        numArray.push({value:random, isHeld:false, id:nanoid()});
+         numArray.push(generateNewDice());
         }
 
       return numArray;
   }
 
-const handleOnHeld=(itemId)=>{
-  console.log("I was clicked",itemId)
-   
-  setDice(oldDice=>oldDice.map(die=>{
-    return die.id===itemId?{...die,isHeld:!die.isHeld}:die
+  const [dice,setDice]=useState(allNewDice);
+  const [tenzies,setTenzies]=useState(false);
+
+  useEffect(()=>{
+    
+   const allHeld =dice.every(dice=>dice.isHeld);
+   const firstValue=dice[0].value;
+   const allSame=dice.every(dice=>dice.value===firstValue);
+
+   if(allHeld && allSame){
+    setTenzies(true)
+    console.log("You have Won")
+   }
+  },[dice])
+
+  const handleOnHeld=(itemId)=>{
+     setDice(oldDice=>oldDice.map(die=>{
+     return die.id===itemId?{...die,isHeld:!die.isHeld}:die
   }))
  
 
 }
 
-  const [dice,setDice]=useState(allNewDice);
 
-  console.log(dice)
 
   const diceElements=dice.map((dice)=><Dice key={dice.id} id={dice.id} handleOnHeld={handleOnHeld} isHeld={dice.isHeld} value={dice.value}/>)
 
   const handleRoll=()=>{
-    setDice(allNewDice());
+
+    setDice((prevState)=>prevState.map(die=>{
+         
+     return die.isHeld?die:generateNewDice()
+    }));
   }
 
   return (
 
     <div className="App">
       <div className='main-container'>
+      <h1 className='heading'>Tenzies</h1>
+        <p className='text'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div className='dice-container'>
         {diceElements}
         </div>
